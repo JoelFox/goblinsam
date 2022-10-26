0cc7814d7f03ce8dadea527675bed56b3dfa55bc
Joel Fox Apelgren <joel.apelgren@makingwaves.com>
2022-10-26 16:17:00 +0200

    Add newsletter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './goblin-sam-transparent.png';
-import './App.css';
+import logo from "./goblin-sam-transparent.png";
+import "./App.css";
 
 function App() {
   return (
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <h1>:goblin-sam:</h1>
         <img src={logo} className="App-logo" alt="logo" />
+        <label for="newsletter">Register to our newsletter</label>
+        <input type="text" placeholder="Your e-mail"></input>
+        <input type="submit" value="Submit"/>
       </header>
     </div>
   );
