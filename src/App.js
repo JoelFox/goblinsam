d437140f21a10cb05c00b398590e983971a59e3b
Joel Fox Apelgren <joel.apelgren@makingwaves.com>
2022-10-27 09:44:40 +0200

    GOBLIN TIME

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from "./goblin-sam-transparent.png";
 import "./App.css";
+import Countdown from 'react-countdown';
+import { useEffect } from "react";
 
 function App() {
+  
+  const d = new Date();
+  const date = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1, 0, 0, 0)
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>:goblin-sam:</h1>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <label for="newsletter">Register to our newsletter</label>
+        {date && <><h4>GOBLIN TIME: </h4><Countdown date={ date } /></> }
+        <label htmlFor="newsletter">Register to our newsletter</label>
         <input type="text" placeholder="Your e-mail"></input>
         <input type="submit" value="Submit"/>
       </header>
