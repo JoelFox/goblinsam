c124c52629c5739e75ab55726b84facff0b10af1
Joel Fox Apelgren <joel.apelgren@makingwaves.com>
2022-11-01 14:20:23 +0100

    More goblins

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import standardLogo from "./goblin-sam-transparent.png";
 import goblinModeLogo from "./goblin-sam-anime.gif";
 import "./App.css";
-import Countdown from 'react-countdown';
-import { useState } from 'react';
+import Countdown from "react-countdown";
+import { useState } from "react";
 
 function App() {
-  
   const d = new Date();
-  const date = new Date(d.getFullYear(), d.getMonth(), d.getHours() >= 17 ? d.getDate() + 1 : d.getDate(), 17, 0, 0);
+  const date = new Date(
+    d.getFullYear(),
+    d.getMonth(),
+    d.getHours() >= 17 ? d.getDate() + 1 : d.getDate(),
+    17,
+    0,
+    0
+  );
   // const date = new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds() + 10);
 
   const [logo, setLogo] = useState(standardLogo);
@@ -15,19 +21,41 @@ function App() {
 
   const onGoblinMode = () => {
     setLogo(goblinModeLogo);
-    setGoblinMode(true)
-  }
+    setGoblinMode(true);
+  };
 
   return (
     <div className={`App ${goblinMode ? "goblinModeReverse" : ""}`}>
       <header className={`App-header ${goblinMode ? "GOBLIN-MODE" : ""}`}>
-        <h1 class={goblinMode ? "goblinModeFast" : ""}>{goblinMode ? "GOBLIN MODE" : ":goblin-sam:"}</h1>
+        <h1 class={goblinMode ? "goblinModeFast" : ""}>
+          {goblinMode ? "GOBLIN MODE" : ":goblin-sam:"}
+        </h1>
+        <div>
+          {goblinMode && (
+            <>
+              <img src={logo} className="App-logo-goblin" alt="logo" />
+              <img src={logo} className="App-logo-goblin" alt="logo" />
+            </>
+          )}
+          <img src={logo} className="App-logo" alt="logo" />
+          {goblinMode && (
+            <>
+              <img src={logo} className="App-logo-goblin" alt="logo" />
+              <img src={logo} className="App-logo-goblin" alt="logo" />
+            </>
+          )}
+        </div>
 
-        <img src={logo} className="App-logo" alt="logo" />
-        {date && <><h4 className={goblinMode ? "goblinMode" : ""}>GOBLIN MODE: <Countdown date={ date } onComplete={onGoblinMode} /></h4></> }
+        {date && (
+          <>
+            <h4 className={goblinMode ? "goblinMode" : ""}>
+              GOBLIN MODE: <Countdown date={date} onComplete={onGoblinMode} />
+            </h4>
+          </>
+        )}
         <label htmlFor="newsletter">Register to our newsletter</label>
         <input type="text" placeholder="Your e-mail"></input>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Submit" />
       </header>
     </div>
   );
