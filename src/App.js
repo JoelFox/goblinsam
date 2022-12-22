b47cedc2c0652f86551019655eb8435aedac51df
Joel Fox Apelgren <joel.apelgren@makingwaves.com>
2022-12-22 15:23:35 +0100

    XMAS UPDATE

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // import standardLogo from "./goblin-sam-transparent.png";
-import goblinModeLogo from "./goblin-sam-anime.gif";
-import luciaLogo from "./goblin-sam-lucia.png";
+// import goblinModeLogo from "./goblin-sam-anime.gif";
+// import luciaLogo from "./goblin-sam-lucia.png";
+import santaLogo from "./goblin-sam-santa.png";
+import reindeerLogo from "./goblin-sam-reindeer.png";
+import audio from "./jingle-bells-8bit.mp3";
 
 import "./App.css";
 import Countdown from "react-countdown";
@@ -8,28 +11,31 @@ import { useState } from "react";
 import Snowfall from "react-snowfall";
 
 function App() {
+  const music = new Audio(audio);
   const d = new Date();
   const date = new Date(
     d.getFullYear(),
     d.getMonth(),
     d.getHours() >= 17 ? d.getDate() + 1 : d.getDate(),
-    17,
-    0,
+    15,
+    30,
     0
   );
   // const date = new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes(), d.getSeconds() + 10);
 
-  const [logo, setLogo] = useState(luciaLogo);
+  const [logo, setLogo] = useState(santaLogo);
   const [goblinMode, setGoblinMode] = useState(false);
   const snowflake1 = document.createElement("img");
-  snowflake1.src = "./goblin-sam-lucia.png";
+  snowflake1.src = "./goblin-sam-santa.png";
 
   const snowFallImages = [snowflake1];
 
   const onGoblinMode = () => {
-    setLogo(goblinModeLogo);
+    // setLogo(goblinModeLogo);
     setGoblinMode(true);
+    music.play();
   };
+
 
   return (
     <>
@@ -37,24 +43,48 @@ function App() {
       <div className={`App ${goblinMode ? "goblinModeReverse" : ""}`}>
         <header className={`App-header ${goblinMode ? "GOBLIN-MODE" : ""}`}>
           <h1 className={goblinMode ? "goblinModeFast" : ""}>
-            {goblinMode ? "GOBLIN MODE" : ":goblin-sam:"}
+            {goblinMode ? "SANTA GOBLIN MODE" : ":goblin-sam:"}
           </h1>
+          {goblinMode && (
+              <>
+              <div>
+              <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+                <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+              </div>
+              <div>
+              <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+                <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+              </div>
+
+              </>
+            )}
           <div>
             {goblinMode && (
               <>
-                <img src={logo} className="App-logo-goblin" alt="logo" />
-                <img src={logo} className="App-logo-goblin" alt="logo" />
+                <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+                <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
               </>
             )}
             <img src={logo} className="App-logo" alt="logo" />
             {goblinMode && (
               <>
-                <img src={logo} className="App-logo-goblin" alt="logo" />
-                <img src={logo} className="App-logo-goblin" alt="logo" />
+                <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+                <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
               </>
             )}
           </div>
-
+          {goblinMode && (
+              <>
+              <div>
+              <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+                <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+              </div>
+              <div>
+              <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+                <img src={reindeerLogo} className="App-logo-goblin" alt="logo" />
+              </div>
+              </>
+            )}
           {date && (
             <>
               <h4 className={goblinMode ? "goblinMode" : ""}>
@@ -65,6 +95,7 @@ function App() {
           <label htmlFor="newsletter">Register to our newsletter</label>
           <input type="text" placeholder="Your e-mail"></input>
           <input type="submit" value="Submit" />
+          {goblinMode && <p>Jingle Bells 8Bit" by Tim Beek (timbeek.com)</p>}
         </header>
       </div>
       <Snowfall images={goblinMode ? snowFallImages : []} radius={goblinMode ? [15, 30] : [3,7]} />
